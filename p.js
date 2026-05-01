01150ba03b96efd84f9eca61643ac37a6ce73a1a
Milanas Orševskis <mil.xomiak@gmail.com>
2026-05-01 15:28:04 +0300

    Update fetch body description for report

--- p.js
+++ p.js
@@ -1,5 +1,5 @@
 fetch('/chall/i/878ac0bc151e117e/report', {
   method: 'POST',
   headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-  body: 'type=rumour&description=' + encodeURIComponent('ADMIN_JS_RAN')
+  body: 'type=rumour&description=' + encodeURIComponent('ADMIN_JS_RAN_ONCE')
 });
