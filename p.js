6ade624414ccac8122bef5cbff3653a0c6160870
Milanas Orševskis <mil.xomiak@gmail.com>
2026-05-01 15:34:08 +0300

    Remove fetch call and add console log
    Removed fetch call for reporting and added a console log.

--- p.js
+++ p.js
@@ -1,5 +1,1 @@
-fetch('/chall/i/878ac0bc151e117e/report', {
-  method: 'POST',
-  headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-  body: 'type=rumour&description=' + encodeURIComponent('ADMIN_JS_RAN_ONCE')
-});
+console.log('loaded');
