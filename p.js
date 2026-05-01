a1cb89ba5c326394ba2501661d06797b72fd0dbb
Milanas Orševskis <mil.xomiak@gmail.com>
2026-05-01 15:23:17 +0300

    Add fetch request to report admin JS execution

---
+++ p.js
@@ -0,0 +1,5 @@
+fetch('/chall/i/878ac0bc151e117e/report', {
+  method: 'POST',
+  headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+  body: 'type=rumour&description=' + encodeURIComponent('ADMIN_JS_RAN')
+});
